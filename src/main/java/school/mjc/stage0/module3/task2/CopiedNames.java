f6e5589620cb82fb8769ad83204fd44836c78dfa
Rico4eT <shiwawa96@mail.ru>
2024-01-19 13:29:04 +0500

    My First Multitasks

--- src/main/java/school/mjc/stage0/module3/task2/CopiedNames.java
+++ src/main/java/school/mjc/stage0/module3/task2/CopiedNames.java
@@ -3,12 +3,12 @@ package school.mjc.stage0.module3.task2;
 public class CopiedNames {
     public static void main(String[] args) {
         int variable = 0;
-        int variable_2 = 2000;
+        int variableTwo = 2000;
         int value = 1000;
-        int variable_3 = 4000;
-        int value_2 = 3000;
+        int variableThree = 4000;
+        int valueTwo = 3000;
         int variables = 5000;
-        int variables_2 = 7000;
-        int value_3 = 90000;
+        int variablesTwo = 7000;
+        int valueThree = 90000;
     }
 }
